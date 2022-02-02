c0e4635e29a845181dd5ac699f9e369d3d7b9dc1
guswntj <guswntj@gmail.com>
2022-02-02 15:44:00 +0900

    first commit

--- src/main/kotlin/com/example/judytest/VaultConfiguration.kt
+++ src/main/kotlin/com/example/judytest/VaultConfiguration.kt
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.vault.annotation.VaultPropertySource
 import org.springframework.vault.config.EnvironmentVaultConfiguration
-
-@Import(EnvironmentVaultConfiguration::class)
-@Configuration
-@VaultPropertySource("\${vault.path}")
-class ValutConfiguration
+//
+//@Import(EnvironmentVaultConfiguration::class)
+//@Configuration
+//@VaultPropertySource("\${vault.path}")
+//class ValutConfiguration
--- src/test/kotlin/com/example/judytest/TempTest.kt
+++ src/test/kotlin/com/example/judytest/TempTest.kt
@@ -2,12 +2,20 @@ package com.example.judytest
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.PropertySource
+
 @SpringBootTest
 class TempTest {
-    @Test
-    fun tempT(){
-        assertThat(1).isEqualTo(1)
 
+    @Value("\${temp.test}")
+    lateinit var test:String
+
+
+    @Test
+    fun tempT() {
+        assertThat(1).isEqualTo(1)
+assertThat(test).isEqualTo("test22")
     }
 }
